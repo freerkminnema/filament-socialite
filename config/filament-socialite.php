3d5f1f1ee928ef90f6ff1b9a889f27ceac6030d5
Freerk Minnema <41089591+freerkminnema@users.noreply.github.com>
2023-07-26 00:20:19 +0200

    Update filament-socialite.php

--- config/filament-socialite.php
+++ config/filament-socialite.php
@@ -27,10 +27,10 @@ return [
     'user_model' => \App\Models\User::class,
 
     // Specify the default redirect route for successful logins
-    'login_redirect_route' => 'filament.pages.dashboard',
+    'login_redirect_route' => 'filament.app.pages.dashboard',
 
     // Specify the route name for the socialite login page
-    'login_page_route' => 'filament.auth.login',
+    'login_page_route' => 'filament.app.auth.login',
     
     // Should the user stay logged in?
     'remember_login' => false,
