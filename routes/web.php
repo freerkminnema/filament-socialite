2bc7b79ee08072c7a35df69338d61e5c8d1fc50c
Freerk Minnema <41089591+freerkminnema@users.noreply.github.com>
2023-08-10 13:17:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,21 @@ use Illuminate\Support\Facades\Route;
 Route::name('filament.')
     ->group(function () {
         foreach (Filament::getPanels() as $panel) {
-            Route::domain($panel->getDomain())
-                ->middleware($panel->getMiddleware())
-                ->name("{$panel->getId()}.socialite.")
-                ->group(function () {
-                    Route::get('/oauth/{provider}', [
-                        SocialiteLoginController::class,
-                        'redirectToProvider',
-                    ])->name('oauth.redirect');
+            foreach ($panel->getDomains() as $domain) {
+                Route::domain($domain)
+                    ->middleware($panel->getMiddleware())
+                    ->name("{$panel->getId()}.socialite.")
+                    ->group(function () {
+                        Route::get('/oauth/{provider}', [
+                            SocialiteLoginController::class,
+                            'redirectToProvider',
+                        ])->name('oauth.redirect');
 
-                    Route::get('/oauth/callback/{provider}', [
-                        SocialiteLoginController::class,
-                        'processCallback',
-                    ])->name('oauth.callback');
-                });
+                        Route::get('/oauth/callback/{provider}', [
+                            SocialiteLoginController::class,
+                            'processCallback',
+                        ])->name('oauth.callback');
+                    });
+            }
         }
     });
